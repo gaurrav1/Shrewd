4d745a0d38e3d82984df1735e1ababd78d600e0c
Gaurav <22amtics009@gmail.com>
2024-08-25 17:03:26 +0530

    Project creation commit

---
+++ Shrewd/src/main/java/com/shrewd/ShrewdApplication.java
@@ -0,0 +1,13 @@
+package com.shrewd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrewdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrewdApplication.class, args);
+	}
+
+}
---
+++ Shrewd/src/test/java/com/shrewd/ShrewdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrewd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShrewdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
